99dcfe990593fddfcbaa42bc3889a238344e102a
Ramiro Meneses Prattes <xanco78@gmail.com>
2018-10-08 21:05:13 -0300

    Added css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,13 @@ socket.on("disconnect", function() {
 jQuery("#message-form").on("submit", function(e){
     e.preventDefault();
 
+    var messageTextBox = jQuery("[name=message]");
+
     socket.emit("createMessage", {
         from: "User",
-        text: jQuery("[name=message]").val()
+        text: messageTextBox.val()
     }, function(){
-
+        messageTextBox.val("");
     })
 });
 
@@ -44,12 +46,16 @@ locationButton.on("click", function() {
         return alert("Geolocation not available");
     }
 
+    locationButton.attr("disabled", "disabled").text("Sending location...");
+
     navigator.geolocation.getCurrentPosition(function(position){
+        locationButton.removeAttr("disabled").text("Send location");
         socket.emit("createLocationMessage", {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, function(){
         alert("Unable to fetch location");
+        locationButton.removeAttr("disabled").text("Send location");
     });
 });
